df59e24010b47a473d4311b35fcfb045f7ea9e40
suthi570 <109793462+suthi570@users.noreply.github.com>
2022-11-03 21:02:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,79 +1,176 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@suthi570 
+KITSCTCLab
+/
+exercise-3-a-array-implementation-of-queue-adt-JOEL1238
+Public
+generated from brightvarghese/22_ODD_DS_Exercise-3.a
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Security
+Insights
+exercise-3-a-array-implementation-of-queue-adt-JOEL1238/Main.py /
+@JOEL1238
+JOEL1238 Update Main.py
+Latest commit 3d9fbd2 2 days ago
+ History
+ 2 contributors
+@JOEL1238@github-classroom
+131 lines (113 sloc)  3.84 KB
+
 class Solution:
-"""This class implements linear queue. Attributes:
-stack: A list which maintains the content of stack. queue: A list which maintains the content of queue.
-top: An integer which denotes the index of the element at the top of the stack. front: An integer which denotes the index of the element at the front of the queue. rear: An integer which denotes the index of the element at the rear of the queue. size: An integer which represents the size of stack and queue.
-"""
+    """This class implements linear queue.
+      Attributes:
+          stack: A list which maintains the content of stack.
+          queue: A list which maintains the content of queue.
+          top: An integer which denotes the index of the element at the top of the stack.
+          front: An integer which denotes the index of the element at the front of the queue.
+          rear: An integer which denotes the index of the element at the rear of the queue.
+          size: An integer which represents the size of stack and queue.
+      """
 
-# Write your code here def  init (self, size):
-"""Inits Solution with stack, queue, size, top, front and rear. Arguments:
-size: An integer to set the size of stack and queue. """
-self.stack = [] self.queue = [] self.size = size self.top = -1
-self.rear = -1
-self.front = -1
+    # Write your code here
+    def __init__(self, size):
+        """Inits Solution with stack, queue, size, top, front and rear.
+        Arguments:
+          size: An integer to set the size of stack and queue.
+        """
+        self.stack = []
+        self.queue = []
+        self.size = size
+        self.top = -1
+        self.rear = -1
+        self.front = -1
 
-def is_stack_empty(self): """
-Check whether the stack is empty. Returns:
-True if it is empty, else returns False. """
-return self.top == -1
+    def is_stack_empty(self):
+        """
+        Check whether the stack is empty.
+        Returns:
+          True if it is empty, else returns False.
+        """
+        return self.top == -1
 
-def is_queue_empty(self): """
-Check whether the queue is empty. Returns:
-True if it is empty, else returns False. """
-return self.front == -1 or self.front > self.rear def is_stack_full(self):
- 
-"""
-Check whether the stack is full. Returns:
-True if it is full, else returns False. """
-return self.top == self.size - 1
+    def is_queue_empty(self):
+        """
+        Check whether the queue is empty.
+        Returns:
+          True if it is empty, else returns False.
+        """
+        return self.front == -1 or self.front > self.rear
 
-def is_queue_full(self): """
-Check whether the queue is full. Returns:
-True if it is full, else returns False. """
-return self.rear == self.size - 1
+    def is_stack_full(self):
+        """
+        Check whether the stack is full.
+        Returns:
+          True if it is full, else returns False.
+        """
+        return self.top == self.size - 1
 
-def push_character(self, character): """
-Push the character to stack, if stack is not full. Arguments:
-character: A character that will be pushed to the stack. """
-if not self.is_stack_full(): self.stack.append(character) self.top += 1
+    def is_queue_full(self):
+        """
+        Check whether the queue is full.
+        Returns:
+          True if it is full, else returns False.  
+        """
+        return self.rear == self.size - 1
 
-def enqueue_character(self, character): """
-Enqueue the character to queue, if queue is not full. Arguments:
-character: A character that will be enqueued to queue. """
-if not self.is_queue_full(): if self.front == -1:
-self.front = 0
-self.rear += 1 self.queue.append(character)
+    def push_character(self, character):
+        """
+        Push the character to stack, if stack is not full.
+        Arguments:
+            character: A character that will be pushed to the stack.
+        """
+        if not self.is_stack_full():
+            self.stack.append(character)
+            self.top += 1
 
-def pop_character(self): """
-Do pop operation if the stack is not empty. Returns:
-The data that is popped out if the stack is not empty. """
-if not self.is_stack_empty(): self.top -= 1
-return self.stack.pop(self.top + 1)
+    def enqueue_character(self, character):
+        """
+        Enqueue the character to queue, if queue is not full.
+        Arguments:
+            character: A character that will be enqueued to queue.
+        """
+        if not self.is_queue_full():
+            if  self.front == -1:
+                self.front = 0
+            self.rear += 1
+            self.queue.append(character)
 
-def dequeue_character(self): """
-Do dequeue operation if the queue is not empty. Returns:
-The data that is dequeued if the queue is not empty. """
-if not self.is_queue_empty():
- 
-self.front += 1
-return self.queue[self.front - 1]
+    def pop_character(self):
+        """
+        Do pop operation if the stack is not empty.
+        Returns:
+          The data that is popped out if the stack is not empty.
+        """
+        if not self.is_stack_empty():
+            self.top -= 1
+            return self.stack.pop(self.top + 1)
+
+    def dequeue_character(self):
+        """
+        Do dequeue operation if the queue is not empty.
+        Returns:
+          The data that is dequeued if the queue is not empty.
+        """
+        if not self.is_queue_empty():
+            self.front += 1
+            return self.queue[self.front - 1] 
+                
 
 
-# read the string text text = input()
+# read the string text
+text = input()
 
-# find the length of text length_of_text = len(text)
+# find the length of text
+length_of_text = len(text)
 
-# Create the Solution class object solution = Solution(length_of_text)
+# Create the Solution class object
+solution = Solution(length_of_text)
 
-# push/enqueue all the characters of string text to stack for index in range(length_of_text):
-solution.push_character(text[index]) solution.enqueue_character(text[index])
-
-is_palindrome = True '''
-pop the top character from stack dequeue the first character from queue compare both characters
-If the comparison fails, set is_palindrome as False. '''
+# push/enqueue all the characters of string text to stack
 for index in range(length_of_text):
-if solution.pop_character() != solution.dequeue_character(): is_palindrome = False
+    solution.push_character(text[index])
+    solution.enqueue_character(text[index])
+
+is_palindrome = True
+'''
+pop the top character from stack
+dequeue the first character from queue
+compare both characters
+If the comparison fails, set is_palindrome as False.
+'''
+for index in range(length_of_text):
+    if solution.pop_character() != solution.dequeue_character():
+        is_palindrome = False
 
 
-# finally print whether string text is palindrome or not. if is_palindrome:
-print("The word, " + text + ", is a palindrome.") else:
-print("The word, " + text + ", is not a palindrome.")
+# finally print whether string text is palindrome or not.
+if is_palindrome:
+    print("The word, " + text + ", is a palindrome.")
+else:
+    print("The word, " + text + ", is not a palindrome.")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+exercise-3-a-array-implementation-of-queue-adt-JOEL1238/Main.py at main · KITSCTCLab/exercise-3-a-array-implementation-of-queue-adt-JOEL1238
